131986affe43429b00b8c93b25eba34069f5a68c
sardar zain <zainsardar193@gmail.com>
2025-10-20 11:44:15 +0500

    js

---
+++ javascript if,if else and if if else.js
@@ -0,0 +1,38 @@
+
+var x = 16;
+if(x%2 ==0)
+{
+    console.log("even")
+}
+if(x%2 != 0)
+{
+    console.log("odd");
+}
+
+
+
+var x = 16
+if(x%2 != 0)
+{
+    console.log("even")
+}
+else
+{
+    console.log("odd");
+}
+
+
+
+var x = 15;
+if(x>100)
+{
+    console.log("first")
+}
+if(x<10)
+{
+    console.log("second")
+}
+else(x==15)
+{
+    console.log("third")
+}
