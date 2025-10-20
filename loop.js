08f5af8726d6afe4d5a23ea7a47e9f84cd7a6e48
sardar zain <zainsardar193@gmail.com>
2025-10-20 21:53:59 +0500

    loops is js

---
+++ loop.js
@@ -0,0 +1,25 @@
+// this is while loop
+var x = 2;
+var i = 3;
+
+while (x <= 7) {
+  console.log(x, "x", i, "=", x * i);
+  x++;
+}
+
+console.log("hi");
+
+// this is for loop
+var x = 5; 
+for (var i = 3; i <= 10; i++) {
+    console.log(x, "x", i, "=", x * i);
+}
+
+// this is do-while loop
+var i = 0;
+do
+{
+    console.log("hello",i)
+    i++
+}
+while(i<3)
